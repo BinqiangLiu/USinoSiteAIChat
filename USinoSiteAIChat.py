187dbf5c889231e74f72ecdfcd32b451734a70d1
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-09-05 13:55:50 +0800

    Update USinoSiteAIChat.py

--- USinoSiteAIChat.py
+++ USinoSiteAIChat.py
@@ -58,9 +58,8 @@ Question: {question}
 AI Repsonse:
 """
 # {input_documents}将作为contexts使用
-PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context", "input_query"]
-)
+
+PROMPT = PromptTemplate(template=prompt_template, input_variables=["input_documents", "input_query"])
 
 #chain = load_qa_chain(llm=llm, chain_type="stuff")
 chain = load_qa_chain(llm=llm, chain_type="stuff", prompt=PROMPT)
